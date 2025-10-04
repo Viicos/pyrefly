264c6beab1ba3cce1546cafa8a69be983b0f508c
Viicos <65306057+Viicos@users.noreply.github.com>
2025-10-04 22:04:43 +0200

    Allow using dict expressions for Pydantic config

--- pyrefly/lib/binding/pydantic.rs
+++ pyrefly/lib/binding/pydantic.rs
@@ -26,6 +26,75 @@ pub const STRICT: Name = Name::new_static("strict");
 const FROZEN: Name = Name::new_static("frozen");
 const EXTRA: Name = Name::new_static("extra");
 
+enum PydanticConfigExpr<'a> {
+    ExprCall(&'a ruff_python_ast::ExprCall),
+    ExprDict(&'a ruff_python_ast::ExprDict),
+}
+
+
+fn filter_keys(item: &ruff_python_ast::DictItem) -> Option<(&str, &Expr)> {
+    match item.key {
+        Some(ref key) => {
+            match key.as_string_literal_expr() {
+                Some(s) => Some((s.value.to_str(), &item.value)),
+                None => None,
+            }
+        },
+        None => None,
+    }
+}
+
+
+impl<'a> PydanticConfigExpr<'a> {
+    fn iter(&self) -> impl Iterator<Item = (&str, &Expr)> {
+        match *self {
+            Self::ExprCall(expr_call) => expr_call
+                .arguments
+                .keywords
+                .iter()
+                .filter_map(|kw| kw.arg.and_then(|arg| Some((arg.id.as_str(), &kw.value)))),
+                Self::ExprDict(expr_dict) => expr_dict.items.iter().filter_map(filter_keys),
+                // Self::ExprDict(expr_dict) => expr_dict.items.iter().filter_map(|item| {
+                //     item.key.and_then(|key| key.as_string_literal_expr().and_then(|s| Some((s.value.to_str(), item.value))))
+                // })
+        }
+
+    }
+}
+
+// enum PydanticConfigExprIter<'a> {
+//     ExprCall(std::slice::Iter<'a, Keyword>),
+//     ExprDict(std::slice::Iter<'a, DictItem>),
+// }
+
+// impl Iterator for PydanticConfigExprIter {
+//     type Item = (&Identifier, &Expr);
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         match self {
+//             Self::ExprCall(kw_iter) => {
+
+//                 let nxt = kw_iter.while_some()
+//             }
+//         }
+//     }
+// }
+
+// impl IntoIterator for PydanticConfigExpr {
+//     type Item = (&Identifier, &Expr);
+//     type IntoIter = std::vec::IntoIter<Self::Item>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         let names = vec!["Jane", "Jill", "Jack", "John"];
+
+//     let total_bytes = names
+//         .iter()
+//         .map(|name: &&str| name.len())
+//         .fold(0, |acc, len| acc + len );
+
+//     }
+// }
+
 /// If a class body contains a `model_config` attribute assigned to a `pydantic.ConfigDict`, the
 /// configuration options from the `ConfigDict`. In the answers phase, this will be merged with
 /// configuration options from the class keywords to produce a full Pydantic model configuration.
@@ -55,6 +124,24 @@ impl Default for PydanticValidationFlags {
 }
 
 impl<'a> BindingsBuilder<'a> {
+    fn get_pydantic_config_expr<'b>(&self, e: &'b Expr) -> Option<PydanticConfigExpr<'b>> {
+        if let Some(call) = e.as_call_expr()
+            && (matches!(
+                self.as_special_export(&call.func),
+                Some(SpecialExport::PydanticConfigDict)
+            ) || call
+                .func
+                .as_name_expr()
+                .is_some_and(|name| name.id == "dict"))
+        {
+            return Some(PydanticConfigExpr::ExprCall(call));
+        } else if let Some(expr_dict) = e.as_dict_expr() {
+            return Some(PydanticConfigExpr::ExprDict(expr_dict));
+        }
+
+        None
+    }
+
     // The goal of this function is to extract pydantic metadata (https://docs.pydantic.dev/latest/concepts/models/) from expressions.
     // TODO: Consider propagating the entire expression instead of the value
     // in case it is aliased.
@@ -65,55 +152,34 @@ impl<'a> BindingsBuilder<'a> {
         pydantic_config_dict: &mut PydanticConfigDict,
     ) {
         if name.as_str() == "model_config"
-            && let Some(call) = e.as_call_expr()
-            && let Some(special) = self.as_special_export(&call.func)
-            && special == SpecialExport::PydanticConfigDict
+            && let Some(pydantic_config_expr) = self.get_pydantic_config_expr(e)
         {
-            for kw in &call.arguments.keywords {
-                if let Some(arg_name) = &kw.arg
-                    && arg_name.id == FROZEN
-                    && let Expr::BooleanLiteral(bl) = &kw.value
+            for (name, value) in pydantic_config_expr.iter() {
+                if name == FROZEN
+                    && let Expr::BooleanLiteral(bl) = value
                 {
                     pydantic_config_dict.frozen = Some(bl.value);
-                }
-
-                if let Some(arg_name) = &kw.arg
-                    && arg_name.id == EXTRA
+                } else if name == EXTRA
+                    && let Some(extra) = value.as_string_literal_expr()
                 {
-                    let config_dict_extra = kw.value.clone().string_literal_expr();
-                    pydantic_config_dict.extra = match config_dict_extra {
-                        Some(extra) => {
-                            let val = extra.value.to_str();
-
-                            if val == "allow" || val == "ignore" {
-                                Some(true)
-                            } else if val == "forbid" {
-                                Some(false)
-                            } else {
-                                None
-                            }
-                        }
-                        None => None,
-                    };
-                }
-
-                if let Some(arg_name) = &kw.arg
-                    && arg_name.id == STRICT
-                    && let Expr::BooleanLiteral(bl) = &kw.value
+                    let extra_value = extra.value.to_str();
+                    pydantic_config_dict.extra = if matches!(extra_value, "allow" | "ignore") {
+                        Some(true)
+                    } else if extra_value == "forbid" {
+                        Some(false)
+                    } else {
+                        None
+                    }
+                } else if name == STRICT
+                    && let Expr::BooleanLiteral(bl) = value
                 {
                     pydantic_config_dict.strict = Some(bl.value);
-                }
-
-                if let Some(arg_name) = &kw.arg
-                    && arg_name.id == VALIDATE_BY_NAME
-                    && let Expr::BooleanLiteral(bl) = &kw.value
+                } else if name == VALIDATE_BY_NAME
+                    && let Expr::BooleanLiteral(bl) = value
                 {
                     pydantic_config_dict.validation_flags.validate_by_name = bl.value;
-                }
-
-                if let Some(arg_name) = &kw.arg
-                    && arg_name.id == VALIDATE_BY_ALIAS
-                    && let Expr::BooleanLiteral(bl) = &kw.value
+                } else if name == VALIDATE_BY_ALIAS
+                    && let Expr::BooleanLiteral(bl) = value
                 {
                     pydantic_config_dict.validation_flags.validate_by_alias = bl.value;
                 }
